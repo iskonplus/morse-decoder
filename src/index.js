dc122faa29786be83d961eb02b424bfa852a93c0
iskonplus <58971909+iskonplus@users.noreply.github.com>
2020-03-08 22:33:10 +0300

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -40,6 +40,21 @@ const MORSE_TABLE = {
 function decode(expr) {
     // write your solution here
 }
+function decode(expr) {
+    let decodedExpr = '',
+        lengthExpr = expr.length;
+
+    for (let i = 0; i < lengthExpr; i += 10) {
+        let letter = expr.slice(i, i + 10)
+            .replace(/00/g, '')
+            .replace(/10/g, '.')
+            .replace(/11/g, '-');
+
+        decodedExpr += MORSE_TABLE[letter] || ' ';
+    }
+
+    return decodedExpr;
+}
 
 module.exports = {
     decode
